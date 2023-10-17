e7462f2a5f817550ba4d6ff964063985dad9f36e
Navid Dorfeshan <navid.dorfeshan@gmail.com>
2023-10-17 12:31:20 +0330

    .

---
+++ Src/BuildingBlocks/Host/Common.Host/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Polly;
+global using Polly.Contrib.WaitAndRetry;
